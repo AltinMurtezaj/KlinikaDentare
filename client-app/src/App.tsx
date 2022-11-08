8d3f9a8a41e34206b86e5462522b03a852e70c66
AltinMurtezaj <93322564+AltinMurtezaj@users.noreply.github.com>
2022-11-08 15:59:46 +0100

    Semantic UI changes !!

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+  const [infermierja, setInfermierja] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/infermierja').then(response =>{
+      console.log(response);
+      setInfermierja(response.data);
+    })
+  }, [])
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header as='h2' icon='users' content='Reactivities' />
+      
+        <List>
+        {infermierja.map((infermierja: any) => (
+            <List.Item key={infermierja.id}>
+              {infermierja.title}
+            </List.Item>
+          ))}
+        </List>
+        
     </div>
   );
 }
