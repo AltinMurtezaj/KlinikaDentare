3fe33e8b33dd1f9681d6ebb9b3d98de859ac10ee
AltinMurtezaj <93322564+AltinMurtezaj@users.noreply.github.com>
2022-11-10 14:38:37 +0100

    asd

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Infermierja } from '../models/infermierja';
 
 function App() {
-  const [infermierja, setInfermierja] = useState([]);
+  const [infermjeret, setInfermierja] = useState<Infermierja>([]);
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/infermierja').then(response =>{
-      console.log(response);
       setInfermierja(response.data);
     })
   }, [])
@@ -22,7 +20,7 @@ function App() {
         <Header as='h2' icon='users' content='Reactivities' />
       
         <List>
-        {infermierja.map((infermierja: any) => (
+        {infermjeret.map((infermierja: any) => (
             <List.Item key={infermierja.id}>
               {infermierja.title}
             </List.Item>
