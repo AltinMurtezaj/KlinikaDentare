adeac220ea351909abd0a39a5e204388655829fc
safetzabelii <sz51042@ubt-uni.net>
2022-11-10 14:57:57 +0100

    commit

---
+++ client-app/src/app/layout/models/infermierja.ts
@@ -0,0 +1,10 @@
+export interface Infermierja {
+    id: number
+    emri: string
+    datelindja: string
+    kualifikimi: string
+    specializimi: string
+    vendbanimi: string
+    nrKontaktues: number
+  }
+  
